e559d8a70efb214ad6c53c8810bbeb1cf1344f57
Chris Young <ctopheryoung@gmail.com>
2022-08-16 15:31:05 -0700

    Initial typescript implementation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,39 @@
 import './App.css';
 import { LineChart, Line, XAxis, YAxis } from 'recharts';
-import data from '../data/selected_formatted_gsodi_data.json'
+import * as data from '../data/selected_formatted_gsodi_data.json'
 
-// NOTE: I had originally written functionality that would handle parsing raw 
-// CSV data into usable JSON, getting the list of selectable countries from
-// that JSON, and more but realized that when reading the data from a local
-// file, it would be more performant to pre-process all the data into formats 
-// that will be needed/useful rather than doing it on the fly.
+interface GSODIDataPoint {
+  ID_country_name: string,
+  ID_country_code: number,
+  ID_year: number,
+  ID_country_year: number,
+  ID_region_name: string,
+  ID_region: number,
+  ID_subregion_name: string,
+  ID_subregion: number,
+  C_A1: number,
+  C_A2: number,
+  C_A3: number,
+  C_A4: number,
+  C_SD51: number,
+  C_SD52: number,
+  C_SD53: number,
+  C_SD54: number,
+  regime_status_name: string,
+  democratic_performance_name: string,
+  democratic_performance_numeric: number,
+}
+
+const dataArray: GSODIDataPoint[] = data as GSODIDataPoint[];
+
+/*
+  NOTE: I had originally written functionality that would handle parsing raw 
+  CSV data into usable JSON, getting the list of selectable countries from
+  that JSON, and more but realized that when reading the data from a local
+  file, it would be more performant (and make for cleaner code) to pre-process
+  all the data into formats that will be needed/useful rather than doing it on
+  the fly.
+*/
 
 // Show cummulative score or show individual lines for each metric?
 // One country or region at a time? With text search box?
@@ -22,9 +49,9 @@ import data from '../data/selected_formatted_gsodi_data.json'
 // C_SD54 - Local Democracy
 
 
-function getFilteredData() {
+function getFilteredData(): Object[] {
   // Perform all filtering operations and return data to show in graph
-  return data.filter(item => item.ID_country_name === "World")
+  return dataArray.filter(item => item.ID_country_name === "World")
 }
 
 function App() {
